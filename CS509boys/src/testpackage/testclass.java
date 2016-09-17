2b2d1d9a021a80da993582162e836c0d2118a0ae
leechenn <leechenn0600@gmail.com>
2016-09-17 00:52:14 -0400

    u

--- CS509boys/src/testpackage/testclass.java
+++ CS509boys/src/testpackage/testclass.java
@@ -4,7 +4,7 @@ public class testclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("328822");
+		System.out.println("3288722");
 	}
 
 }
