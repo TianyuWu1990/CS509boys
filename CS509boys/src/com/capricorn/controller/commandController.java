dc6677519ce643c796fa485e30fcab92c40db03c
TianyuWu1990 <wty773752792@gmail.com>
2016-10-11 00:43:19 -0400

    add controller, dont know how to move actionlistener out of jframe

---
+++ CS509boys/src/com/capricorn/controller/commandController.java
@@ -0,0 +1,19 @@
+package com.capricorn.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import com.capricorn.view.*;
+
+public class commandController implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
