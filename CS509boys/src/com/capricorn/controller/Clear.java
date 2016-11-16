bec3425f030a8b2d85a8df084a7d6730d76c1294
sbw <sbw@localhost>
2016-11-16 18:18:27 -0500

    clear doc

--- CS509boys/src/com/capricorn/controller/Clear.java
+++ CS509boys/src/com/capricorn/controller/Clear.java
@@ -9,11 +9,18 @@ import com.capricorn.view.PracticeGame;
  * Responsible for clear all the letters in the board field.
  */
 public class Clear implements ActionListener{
+	/**
+	 * Here is a constructor for Clear in order to make a fault value.
+	 * Construct Clear object to use default port number.
+	 */
 	PracticeGame p;
 	public Clear(PracticeGame p){
 		this.p=p;
 	}
-
+	/** Override the the actionPerformed, so under the conditions that
+	 *  Firstly nothing exists so we set null
+	 *  Secondly else if there can be got something like letters we set it to 0;
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (p.txtOwn.getText().length() != 0) {
@@ -21,6 +28,7 @@ public class Clear implements ActionListener{
 		}
 		p.textField.setText("0");
 		p.sum = 0;
+		//loop to set the button true
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				p.btnArray[i][j].setEnabled(true);;
