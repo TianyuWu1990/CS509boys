6ef9100d95acf13e8e6ecf68f3cff8d11bd900b3
sbw <sbw@localhost>
2016-11-16 18:59:26 -0500

    test of two

--- CS509boys/test/com/capricorn/view/TestPracticeGame.java
+++ CS509boys/test/com/capricorn/view/TestPracticeGame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import com.capricorn.entity.Board;
 
 public class TestPracticeGame {
-
+/**main function to test the PracticeGame model*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
--- CS509boys/test/com/capricorn/view/TestStartPage.java
+++ CS509boys/test/com/capricorn/view/TestStartPage.java
@@ -5,11 +5,8 @@ import javax.swing.JFrame;
 import com.capricorn.entity.Board;
 import com.capricorn.model.Model;
 
-
-
-
 public class TestStartPage {
-
+/**main function the start the single game*/
 	public static void main(String[] args) {
 		//Model m = new Model();
 		// TODO Auto-generated method stub
