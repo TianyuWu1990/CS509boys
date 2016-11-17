0dd6b443f54e9f2fd8c3fddc25ea1c38720a2123
sbw <sbw@localhost>
2016-11-16 19:08:44 -0500

    11

--- CS509boys/src/com/capricorn/entity/Board.java
+++ CS509boys/src/com/capricorn/entity/Board.java
@@ -1,8 +1,10 @@
 package com.capricorn.entity;
-
+/**As defined, the size of the board is should be 4-letters'-long and 4-letters'-wide.
+  *Applying cell to generate a 4*4 board with 16 letters in it.
+  */
 public class Board {
 public Cell letterArray[][] = new Cell[4][4];
-
+/**SetLetter method to fill in the cell with different word*/
 public void setcellArray(){
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
