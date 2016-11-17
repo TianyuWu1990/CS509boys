8d248867084c2c4eb78e082f7fdb805fcfbde88e
sbw <sbw@localhost>
2016-11-16 19:42:25 -0500

    123123123

--- CS509boys/src/com/capricorn/controller/Exit.java
+++ CS509boys/src/com/capricorn/controller/Exit.java
@@ -10,14 +10,16 @@ import com.capricorn.entity.Board;
 import com.capricorn.model.Model;
 import com.capricorn.view.PracticeGame;
 import com.capricorn.view.StartPage;
-
+/**Exit controller implement actionlistener.*/
 public class Exit implements ActionListener{
 	JFrame j;
 	Model m;
+/**Here is a constructor for Exit in order to make a fault value.
+	 * Construct Exit object to use default port number.*/
 	public Exit(JFrame j){
 		this.j=j;
 	}
-
+/**Dispose the interface and shut down the software.*/
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		j.dispose();
