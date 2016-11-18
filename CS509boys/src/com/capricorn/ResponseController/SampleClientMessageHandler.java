6b76564617aafa23f4a70bb33fd6e5498f697040
leechenn <leechenn0600@gmail.com>
2016-11-18 00:39:06 -0500

    some changes in handled

--- CS509boys/src/com/capricorn/ResponseController/SampleClientMessageHandler.java
+++ CS509boys/src/com/capricorn/ResponseController/SampleClientMessageHandler.java
@@ -1,18 +1,41 @@
 package com.capricorn.ResponseController;
+import com.capricorn.RequestController.ControllerChain;
+import com.capricorn.RequestController.EmptyHandler;
 import com.capricorn.client.IMessageHandler;
+import com.capricorn.view.Application;
 
 import xml.Message;
 
 /**
  * Sample implementation of a protocol handler to respond to messages received from the server.
  * You should follow this template when designing YOUR message handler.
+ * 
+ * Note: This one does nothing more than dump the XML message into the client window. Nothing that special.
  */
 public class SampleClientMessageHandler implements IMessageHandler {
 
+	Application app;
+	
+	// by default is the empty handler...
+	ControllerChain chain = new EmptyHandler();
+	
+	/**
+	 * Register new controller chain as occuring before existing chain.
+	 */
+	public void registerHandler(ControllerChain handler) {
+		handler.next = chain;
+		chain = handler;
+	}
+	
+	public SampleClientMessageHandler(Application app) {		this.app = app;
+	}
+	
 	@Override
 	public void process(Message response) {
-
-		System.out.println("Received:" + response);
+		chain.process(response);
 	}
+	
+	
+	
 
 }
--- CS509boys/src/com/capricorn/client/ClientLauncher.java
+++ CS509boys/src/com/capricorn/client/ClientLauncher.java
@@ -1,6 +1,8 @@
 package com.capricorn.client;
-import java.util.Scanner;
+import javax.swing.UIManager;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 
+import com.capricorn.RequestController.ConnectResponseController;
 import com.capricorn.ResponseController.SampleClientMessageHandler;
 import com.capricorn.model.Model;
 import com.capricorn.view.Application;
@@ -10,54 +12,62 @@ import xml.Message;
 
 /** Launch command-line Client to show minimal access needs. */
 public class ClientLauncher {
-	
+
 	// If requested by ClientLauncher (pass in '-server' as argument).
 	public static final String serverHost = "72.249.186.243";
-
+	
 	/**
 	 * Note that to simplify the coding of this command-client, it declares that it can throw an Exception,
 	 * which is typically the failed connection to a server.
 	 */
-	
-		// FIRST thing to do is register the protocol being used. 
-		// There will be a single class protocol, we have set it as wordsweeper
-		public static void main(String[] args) throws Exception {
-			// FIRST thing to do is register the protocol being used. There will be a single class protocol
-			// that will be defined and which everyone will use. For now, demonstrate with skeleton protocol.
-			if (!Message.configure("wordsweeper.xsd")) {
-				System.exit(0);
-			}
-			
-			ServerAccess sa = new ServerAccess("localhost");
-			Model model=new Model();
-			Application a=new Application(model);
-			sa.connect(new SampleClientMessageHandler());
-			
-			// send an introductory connect request 
-			Message m = new Message ("<request id='l23kjlkasjldksljad'><connectRequest/></request>");
-			
-			sa.sendRequest(m);
-			
-			// await response. If we don't stop ServerAccess manually, there will be a background thread
-			// the continually runs and the program will never terminate. 
-			// This problem is not as noticeable in GUI-based clients.
-			System.err.println("Press Return to terminate client.");
-			Scanner sc = new Scanner (System.in);
-			sc.nextLine();
-			
-			System.out.println("Client disconnected.");
-			sa.disconnect();
-		} 
-	
-		// await response. If we don't stop ServerAccess manually, there will be a background thread
-		// the continually runs and the program will never terminate. 
-		// This problem is not as noticeable in GUI-based clients.
-//		System.err.println("Press Return to terminate client.");
-//		Scanner sc = new Scanner (System.in);
-//		sc.nextLine();
-//		
-//		System.out.println("Client disconnected.");
-//		sa.disconnect();
+	public static void main(String[] args) throws Exception {
 		
-	} 
+		UIManager.setLookAndFeel(new MetalLookAndFeel());
+		
+		// FIRST thing to do is register the protocol being used. There will be a single class protocol
+		// that will be defined and which everyone will use. For now, demonstrate with skeleton protocol.
+		if (!Message.configure("wordsweeper.xsd")) {
+			System.exit(0);
+		}
+		
+		// select dedicated server with '-server' options
+		String host = "localhost";
+		if (args.length > 0 && args[0].equals("-server")) {
+			host = serverHost;
+		}
+		
+		// Initialize the client application and its corresponding model
+		Model model = new Model();
+		Application app = new Application(model);
+		
+		SampleClientMessageHandler handler = new SampleClientMessageHandler(app);
+		
+		
 
+
+			
+		// try to connect to the server. Once connected, messages are going to be processed by 
+		// SampleClientMessageHandler. For now we just continue on with the initialization because
+		// no message is actually sent by the connect method.
+		ServerAccess sa = new ServerAccess(host, 11425);
+		if (!sa.connect(handler)) {
+			System.out.println("Unable to connect to server (" + host + "). Exiting.");
+			System.exit(0);
+		}
+		System.out.println("Connected to " + host);
+		
+		
+		// Should we on the client ever need to communicate with the server, we need this ServerAccess
+		// object.
+		app.setServerAccess(sa);
+		// send an introductory connect request now that we have created (but not made visible!)
+		// the GUI
+		String xmlString = Message.requestHeader() + "<connectRequest/></request>";
+		Message m = new Message (xmlString);
+		sa.sendRequest(m);
+		
+		
+		// at this point, we need to make app visible, otherwise we would terminate application
+		app.setVisible(true);
+	} 
+}
