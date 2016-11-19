1edb64288bdaa721aa58e1903089ad025b93c2fd
leechenn <leechenn0600@gmail.com>
2016-11-18 22:13:04 -0500

    change in model

--- CS509boys/src/com/capricorn/entity/Board.java
+++ CS509boys/src/com/capricorn/entity/Board.java
@@ -13,6 +13,8 @@ public class Board {
 public Cell cells[][] = new Cell[4][4];
 private ArrayList<Cell> chosenCells;
 private Word word;
+private int globalStartingCol;
+private int globalStartingRow;
 
 /**SetLetter method to fill in the cell with different word*/
 public void setcellArrayForPractice(){
@@ -25,7 +27,8 @@ public void setcellArrayForPractice(){
 }
 
 public Board(){
-	
+	globalStartingCol=0;
+	globalStartingRow=0;
 	chosenCells = new ArrayList<Cell>();
 	word = new Word();
 	
@@ -42,8 +45,96 @@ public Board(){
 		}
 	}
 }
-public void updateBoard(){
+
+public void updateBoard(int newStartingCol, int newStaringRow, String NewBoardInfo){
+	this.globalStartingCol = newStartingCol;
+	this.globalStartingRow = newStaringRow;
+	for(int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+           int x=globalStartingCol+i;
+           int y=globalStartingRow+j;
+           Coordinate globalCoordinate=new Coordinate(x,y);
+           cells[i][j].setGlobalCoordinate(globalCoordinate);
+	}
+		}
+	
+
+	updateBoardByAllLetters(NewBoardInfo);
+}
+
+private void updateBoardByAllLetters(String cellsLetters){
+	String[] cellInforList = cellsLetters.split(",");
+	for(int i = 0; i < 16; i++){
+		int row=i/4;
+		int col=i%4;
+		if(cellInforList[i]=="Q")
+			cellInforList[i]="Qu";
+		cells[row][col].setLetter(cellInforList[i]);;
+	}
+}
+
+public String getBoardInfo(){
+	String allCellLetters = "";
+	for(int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			allCellLetters+=cells[i][j].getLetter();
+	}
+		}
+	return allCellLetters;
+}
+
+public ArrayList<Cell> getChosenCells() {
+	return chosenCells;
+}
+
+public void setChosenCells(ArrayList<Cell> chosenCells) {
+	this.chosenCells = chosenCells;
+}
+
+public Word getWord() {
+	return word;
+}
+
+public void setWord(Word word) {
+	this.word = word;
+}
+
+public int getGlobalStartingCol() {
+	return globalStartingCol;
+}
+
+public void setGlobalStartingCol(int globalStartingCol) {
+	this.globalStartingCol = globalStartingCol;
+}
+
+public int getGlobalStartingRow() {
+	return globalStartingRow;
+}
+
+public void setGlobalStartingRow(int globalStartingRow) {
+	this.globalStartingRow = globalStartingRow;
+}
+
+public Cell[][] getCells() {
+	return cells;
+}
+public String getChosenCellsXMLString(){
+	String chosenCellsString = "";
+	for(int i=0;i<4;i++){
+		for (int j=0;j<4;j++){
+			chosenCellsString += String.format("<cell position='%s,%s' letter='%s'/>", 
+					String.valueOf(cells[i][j].getGlobalCoordinate().row), 
+					String.valueOf(cells[i][j].getGlobalCoordinate().col), 
+					cells[i][j].getLetter());			
+	}
+		}
+	return chosenCellsString;
+}
 	
 }
-}
+
+
+
+
+	
 
