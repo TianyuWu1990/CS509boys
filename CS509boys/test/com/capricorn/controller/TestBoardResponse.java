d0bd2a18771ae7ed5e63c28b07e81bcbb7db1d5d
TianyuWu1990 <wty773752792@gmail.com>
2016-12-08 00:20:45 -0500

    Add controller test

--- CS509boys/test/com/capricorn/controller/TestBoardResponse.java
+++ CS509boys/test/com/capricorn/controller/TestBoardResponse.java
@@ -3,5 +3,6 @@ package com.capricorn.controller;
 import junit.framework.TestCase;
 
 public class TestBoardResponse extends TestCase {
+	
 
 }
--- CS509boys/test/com/capricorn/controller/TestExitGameResponse.java
+++ CS509boys/test/com/capricorn/controller/TestExitGameResponse.java
@@ -1,7 +1,57 @@
 package com.capricorn.controller;
 
+import java.util.ArrayList;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import com.capricorn.RequestController.ExitGameRequest;
+import com.capricorn.ResponseController.ExitGameResponse;
+import com.capricorn.client.ServerAccess;
+import com.capricorn.model.Model;
+import com.capricorn.view.Application;
+
 import junit.framework.TestCase;
+import xml.Message;
 
 public class TestExitGameResponse extends TestCase {
+	Model model = new Model();
+	Application app = new Application(model);
+	ServerAccess sa=new ServerAccess("localhost");
+	
+	public void set()
+	{
+		// FIRST thing to do is register the protocol being used.
+		if (!Message.configure("wordsweeper.xsd")) {
+			fail ("unable to configure protocol");
+		}
+		app.setVisible(true);
+		app.setServerAccess(sa);
+	}
+
+
+
+
+		public void TestExitGameProcess() {
+			
+			
+			
+			String player = "player";
+			String gameid = "gameid";
+			model.getPlayer().setName(player);
+			model.getGame().setGameId(gameid);
+			
+			
+			
+			ExitGameRequest exitreq = new ExitGameRequest(model,app);
+			exitreq.process();
+			
+			String r = app.getXmlb().getMessageInfo().getText();
+			
+			System.out.println(r);
+			//assertEquals(1,1);
+
+	}
+		
 
 }
