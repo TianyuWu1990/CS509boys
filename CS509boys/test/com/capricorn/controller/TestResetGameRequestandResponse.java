e8ef29020cd03d3227d0691431aeb97d4d0ba8ef
yli14 <yli14@wpi.edu>
2016-12-08 00:50:34 -0500

    123

---
+++ CS509boys/test/com/capricorn/controller/TestResetGameRequestandResponse.java
@@ -0,0 +1,25 @@
+package com.capricorn.controller;
+
+import xml.Message;
+import junit.framework.TestCase;
+
+import com.capricorn.RequestController.ResetGameRequest;
+import com.capricorn.ResponseController.*;
+import com.capricorn.client.ServerAccess;
+import com.capricorn.model.Model;
+import com.capricorn.view.Application;
+
+public class TestResetGameRequestandResponse extends TestCase {
+	
+	Model model1 = new Model();
+	Application app1= new Application(model1);
+	ServerAccess sa;
+
+	
+public void testprocess(){
+
+	
+	
+}
+
+}
