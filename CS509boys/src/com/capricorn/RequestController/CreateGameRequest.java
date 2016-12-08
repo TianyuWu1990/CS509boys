7998054b95c3f9d79c104d0ceb439e1701b3962c
leechenn <leechenn0600@gmail.com>
2016-12-08 02:13:42 -0500

    request

---
+++ CS509boys/src/com/capricorn/RequestController/CreateGameRequest.java
@@ -0,0 +1,53 @@
+package com.capricorn.RequestController;
+
+
+import com.capricorn.model.Model;
+import com.capricorn.view.Application;
+
+import xml.Message;
+
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class CreateGameRequest.
+ */
+public class CreateGameRequest{
+
+    /** The app. */
+    Application app;
+    
+    /** The model. */
+    Model model;
+
+	/**
+	 * Instantiates a new creates the game request.
+	 *
+	 * @param app the app
+	 * @param model the model
+	 */
+	public CreateGameRequest(Application app,Model model) {
+		this.app = app;
+		//this.model=model;
+		
+	}
+
+
+/**
+ * Process.
+ */
+public void process() {
+		
+		String xmlString;
+		if(this.app.getPassword() == null){
+			xmlString = Message.requestHeader() + String.format("<createGameRequest name='%s'/></request>", this.app.getPlayName());		
+		}else{
+			xmlString = Message.requestHeader() + String.format("<createGameRequest name='%s' password='%s'/></request>", this.app.getPlayName(), this.app.getPassword());
+		}		
+		Message m = new Message (xmlString);
+		app.getServerAccess().sendRequest(m);
+		app.getXmlb().getMessageInfo().append(m.toString()+"\n");
+		
+	}
+
+}
