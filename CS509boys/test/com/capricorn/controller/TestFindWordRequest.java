ba8ba5e2f0759408bfe6892792cf2b1d154c41b3
yli14 <yli14@wpi.edu>
2016-12-10 01:42:27 -0500

    Request Controller test

---
+++ CS509boys/test/com/capricorn/controller/TestFindWordRequest.java
@@ -0,0 +1,44 @@
+package com.capricorn.controller;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import xml.Message;
+
+import com.capricorn.RequestController.FindWordRequest;
+import com.capricorn.entity.Cell;
+import com.capricorn.entity.Model;
+import com.capricorn.mockServer.MockServer;
+import com.capricorn.view.Application;
+
+public class TestFindWordRequest extends TestCase{
+
+	Model model = new Model();
+	Application app = new Application(model);
+	MockServer ms = new MockServer("localhost");
+	
+	
+	public void testprocess() throws Exception{
+		
+		if (!Message.configure("wordsweeper.xsd")) { 
+			fail ("unable to configure protocol"); 
+		}
+		app.setServerAccess(ms);
+		FindWordRequest req = new FindWordRequest(model, app);
+		model.getPlayer().setName("yu");
+		model.getGame().setGameId("fortest");
+		
+		Cell ce1 = new Cell();
+		ArrayList<Cell> Chosencells= new ArrayList<Cell>();
+		Chosencells.add(ce1);
+		model.getBoard().setChosenCells(Chosencells);
+		model.getBoard().getWord().setContent("abc");
+		
+		req.process();
+		ArrayList<Message> al = new ArrayList<Message>();
+		al = ms.getAndClearMessages();
+		assertEquals(al.get(0).contents.getFirstChild().getLocalName(),"findWordRequest");
+		assertEquals(al.get(0).contents.getFirstChild().getAttributes().getNamedItem("gameId").getNodeValue(),model.getGame().getGameId());
+		assertEquals(al.get(0).contents.getFirstChild().getAttributes().getNamedItem("name").getNodeValue(),model.getPlayer().getName());
+	}
+}
--- CS509boys/test/com/capricorn/controller/TestLockGameRequest.java
+++ CS509boys/test/com/capricorn/controller/TestLockGameRequest.java
@@ -1,42 +1,34 @@
 package com.capricorn.controller;
 
 import java.util.ArrayList;
-
+import junit.framework.TestCase;
 import xml.Message;
-
-import com.capricorn.RequestController.ResetGameRequest;
+import com.capricorn.RequestController.LockGameRequest;
 import com.capricorn.entity.Model;
-import com.capricorn.mockServer.*;
+import com.capricorn.mockServer.MockServer;
 import com.capricorn.view.Application;
 
-import junit.framework.TestCase;
+public class TestLockGameRequest extends TestCase{
 
-public class TestResetGameRequest extends TestCase {
-	
 	Model model = new Model();
 	Application app = new Application(model);
 	MockServer ms = new MockServer("localhost");
-			
+	
 	
 	public void testprocess() throws Exception{
-	
+		
 		if (!Message.configure("wordsweeper.xsd")) { 
 			fail ("unable to configure protocol"); 
 		}
 		app.setServerAccess(ms);
-		ResetGameRequest req = new ResetGameRequest(model, app);
-		model.getGame().setGameId("forTest");
-	
+		LockGameRequest req = new LockGameRequest(model, app);
+		model.getPlayer().setName("lee");
+		model.getGame().setGameId("fortest");
+		
 		req.process();
-	
 		ArrayList<Message> al = new ArrayList<Message>();
 		al = ms.getAndClearMessages();
-		assertEquals(al.get(0).contents.getFirstChild().getLocalName(),"resetGameRequest");
+		assertEquals(al.get(0).contents.getFirstChild().getLocalName(),"lockGameRequest");
 		assertEquals(al.get(0).contents.getFirstChild().getAttributes().getNamedItem("gameId").getNodeValue(),model.getGame().getGameId());
-	
-	
-	
-	
-	
 	}
 }
