90b9f3024ef53c9d5d22c09af7c0b4f25bc078d8
TianyuWu1990 <wty773752792@gmail.com>
2016-12-10 01:29:00 -0500

    add boundaries test coverage75%

--- CS509boys/test/com/capricorn/boundary/TestPracticeGame.java
+++ CS509boys/test/com/capricorn/boundary/TestPracticeGame.java
@@ -1,10 +1,18 @@
 package com.capricorn.boundary;
 import com.capricorn.entity.Model;
+import com.capricorn.view.Application;
+import com.capricorn.view.MultiGame;
 import com.capricorn.view.PracticeGame;
 import junit.framework.TestCase;
 
 public class TestPracticeGame extends TestCase {
-	Model m;
-	PracticeGame a = new PracticeGame(m);
+	public void testprocess() throws Exception{
+		Model model = new Model();
+		Application app = new Application(model);
+		PracticeGame prac = new PracticeGame(model);
+		prac.setVisible(true);
+		assertTrue(prac.isVisible());
+
+		}
     
 }
