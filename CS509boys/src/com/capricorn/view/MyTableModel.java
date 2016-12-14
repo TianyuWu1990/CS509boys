3fb75223eb9f6971145c099848b5ae346073bcb7
sbw <sbw@localhost>
2016-12-13 21:45:47 -0500

    editable doc

--- CS509boys/src/com/capricorn/view/MyTableModel.java
+++ CS509boys/src/com/capricorn/view/MyTableModel.java
@@ -3,10 +3,14 @@ package com.capricorn.view;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 
+/**Model the table and make cell in the table can not be editable*/
 public class MyTableModel extends DefaultTableModel {
 	MyTableModel(Object[][] data, String[] columnNames) {
 	        super(data, columnNames);
 	    }
+	/**Override the method isCelleditable 
+	 * And return false
+	 */
  @Override
     public boolean isCellEditable(int row, int column){  
         return false;  
